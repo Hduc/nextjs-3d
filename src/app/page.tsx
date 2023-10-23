2058cd2bce9b00f6f4db7e3e21f32627a875b56b
Nguyen Hoang Duc <duc.nguyen@minhlongi.com>
2023-10-23 10:32:03 +0700

    @google/model-viewer

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+
+"use client"
+import './globals.css'
+import '@google/model-viewer'
+
+export default function Home() {
+    {/*  */}
+  return (
+    //@ts-ignore
+    <model-viewer
+      id="view3d"
+      src="/3d/near.glb"
+      shadow-intensity="1"
+      shadow-softness="0.35"
+      ar
+      camera-controls
+      touch-action="pan-y"
+      auto-rotate
+    >
+      <button slot="ar-button">
+        View in your space
+      </button>
+      {/* @ts-ignore */}
+    </model-viewer>
+  )
+}
